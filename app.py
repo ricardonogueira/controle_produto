e7222bcd81ba68c9ee6f17f93eb85108eab44a90
Ricardo Nogueira de Figueiredo <ricardoogrande@gmail.com>
2025-03-03 12:08:05 -0300

    Neste commit foi feito implementado registro de usuários no sistema e autenticação. Para isso foi utilizado Blueprints.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from gerenciamento_prod import app_prod
+from autenticacao_usuario import app_auth
+from registrar_usuarios import app_registro
 import config
 
 app = Flask(__name__)
@@ -8,3 +10,5 @@ app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 app.secret_key = config.SECRET_KEY
 
 app.register_blueprint(app_prod)
+app.register_blueprint(app_auth)
+app.register_blueprint(app_registro)
--- gerenciamento_prod.py
+++ gerenciamento_prod.py
@@ -1,26 +1,26 @@
-from flask import Blueprint, render_template, request, redirect, flash
+from flask import Blueprint, render_template, request, redirect, flash, session, url_for
 from werkzeug.utils import secure_filename
 import config
 import json
 import os
 
-app_prod = Blueprint('cad_prod',__name__)
+app_prod = Blueprint('cad_prod',__name__, template_folder='templates/cadastro/', url_prefix='/produtos')
 
 produtos = []
 
 def gravar_produtos_arquivo():
     
     produtos_json = json.dumps(produtos)
-    with open('banco.json', 'w') as arq:
+    with open('produtos.json', 'w') as arq:
         arq.write(produtos_json)
 
 def ler_arquivo_json():
     global produtos
-    with open('banco.json', 'r') as arq:
+    with open('produtos.json', 'r') as arq:
             produtos = json.load(arq)
 
 def escrever_arquivo_json():
-     with open('banco.json', 'w+') as arq:
+     with open('produtos.json', 'w+') as arq:
             arq.write("[]")
 
 def carregar_produtos_arquivo():
@@ -47,88 +47,104 @@ def buscar_indice_produto(id):
                 return indice, produto
     return -1, ""
 
+def validar_sessao():
+    try:
+        if session['logado']:
+            return True
+    except:
+        return False
+
 @app_prod.route('/')
 def home():
-    return render_template('cadastro/home.html', produtos=produtos)
+    if validar_sessao():
+        return render_template('home.html', produtos=produtos)
+    
+    return redirect( url_for('autenticacao.login') )
 
 @app_prod.route('/adicionar', methods=['GET', 'POST'])
 def adicionar():
-    if request.method == "POST":
-        nome = request.form['nome']
-        preco = float(request.form['preco'])
-        quantidade = int(request.form['quantidade'])
+    if validar_sessao():
+        if request.method == "POST":
+            nome = request.form['nome']
+            preco = float(request.form['preco'])
+            quantidade = int(request.form['quantidade'])
 
-        nome_arquivo = 'padrao.jpg'
-        if 'foto' in request.files:
-            foto = request.files['foto']
-            if foto.filename != '':
-                if foto and extensoes_permitidas(foto.filename):
-                    nome_arquivo = secure_filename(foto.filename)
-                    foto.save(os.path.join(config.UPLOAD_FOLDER, nome_arquivo))
+            nome_arquivo = 'padrao.jpg'
+            if 'foto' in request.files:
+                foto = request.files['foto']
+                if foto.filename != '':
+                    if foto and extensoes_permitidas(foto.filename):
+                        nome_arquivo = secure_filename(foto.filename)
+                        foto.save(os.path.join(config.UPLOAD_FOLDER, nome_arquivo))
 
-        
-        id = ultimo_id_inserido()
+            
+            id = ultimo_id_inserido()
 
-        produto = {
-            "id": (id + 1), 
-            "nome": nome, 
-            "preco": preco,
-            "quantidade": quantidade, 
-            "foto": nome_arquivo
-        }
+            produto = {
+                "id": (id + 1), 
+                "nome": nome, 
+                "preco": preco,
+                "quantidade": quantidade, 
+                "foto": nome_arquivo
+            }
 
-        produtos.append(produto)
+            produtos.append(produto)
+            gravar_produtos_arquivo()
+            flash("Produto Cadastrado com Sucesso")
 
-        gravar_produtos_arquivo()
-
-        flash("Produto Cadastrado com Sucesso")
-        return redirect('/')
-    return render_template('cadastro/adicionar.html')
+            return redirect('/')
+        return render_template('adicionar.html')
+    return redirect( url_for('autenticacao.login') )
 
 @app_prod.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar(id):
 
-    indice, produto_selecionado = buscar_indice_produto(id)
+    if validar_sessao():
+        indice, produto_selecionado = buscar_indice_produto(id)
 
-    if request.method == "POST":
+        if request.method == "POST":
 
-        nome_arquivo = request.form['imagem']
-        if 'foto' in request.files:
-            foto = request.files['foto']
-            if foto.filename != '':
-                if foto and extensoes_permitidas(foto.filename):
-                    nome_arquivo = secure_filename(foto.filename)
-                    foto.save(os.path.join(config.UPLOAD_FOLDER, nome_arquivo))
+            nome_arquivo = request.form['imagem']
+            if 'foto' in request.files:
+                foto = request.files['foto']
+                if foto.filename != '':
+                    if foto and extensoes_permitidas(foto.filename):
+                        nome_arquivo = secure_filename(foto.filename)
+                        foto.save(os.path.join(config.UPLOAD_FOLDER, nome_arquivo))
 
-        print(nome_arquivo)
-        if indice != -1:
-            produto = {
-                "id": id, 
-                "nome": request.form['nome'],
-                "preco": float(request.form['preco']), 
-                "quantidade": int(request.form['quantidade']),
-                "foto": nome_arquivo
-            }
-            produtos[indice] = produto
-        
-        gravar_produtos_arquivo()
-        flash("Produto Editado com Sucesso")
-
-        return redirect('/')
+            print(nome_arquivo)
+            if indice != -1:
+                produto = {
+                    "id": id, 
+                    "nome": request.form['nome'],
+                    "preco": float(request.form['preco']), 
+                    "quantidade": int(request.form['quantidade']),
+                    "foto": nome_arquivo
+                }
+                produtos[indice] = produto
             
-    produto = {
-        "id" : produto_selecionado['id'],
-        "nome" : produto_selecionado['nome'],
-        "preco" : produto_selecionado['preco'],
-        "quantidade" : produto_selecionado['quantidade'],
-        "foto" : produto_selecionado['foto'],
-    }
+            gravar_produtos_arquivo()
+            flash("Produto Editado com Sucesso")
 
-    return render_template('cadastro/editar.html', produto=produto)
+            return redirect('/')
+                
+        produto = {
+            "id" : produto_selecionado['id'],
+            "nome" : produto_selecionado['nome'],
+            "preco" : produto_selecionado['preco'],
+            "quantidade" : produto_selecionado['quantidade'],
+            "foto" : produto_selecionado['foto'],
+        }
+        return render_template('editar.html', produto=produto)
+    
+    return redirect( url_for('autenticacao.login') )
 
 @app_prod.route('/excluir', methods=['POST'])
 def excluir():
-    indice, _ = buscar_indice_produto(int(request.form['id']))
-    produtos.pop(indice)
-    flash("Produto Excluído com Sucesso")
-    return redirect('/')
+    if validar_sessao():
+        indice, _ = buscar_indice_produto(int(request.form['id']))
+        produtos.pop(indice)
+        flash("Produto Excluído com Sucesso")
+        return redirect('/')
+    
+    return redirect( url_for('autenticacao.login') )
